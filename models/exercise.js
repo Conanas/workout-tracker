41aa376777fa008095d53cd5b0def3f3edae481a
Tom Haigh <tmhaigh@gmail.com>
2020-12-02 10:30:05 +1100

    initial exercises database schema file

---
+++ models/exercise.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    // name: {
+    //     type: String,
+    //     trim: true,
+    //     required: "Enter a name for transaction"
+    // },
+    // value: {
+    //     type: Number,
+    //     required: "Enter an amount"
+    // },
+    // date: {
+    //     type: Date,
+    //     default: Date.now
+    // }
+    // day: {
+    //     type: Date,
+    //     default: Date.now
+    // },
+    // exercises: 
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
