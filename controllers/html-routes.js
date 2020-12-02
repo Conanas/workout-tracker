308c4a1501b321b17e2ccf2cef823e32fa9f9188
Tom Haigh <tmhaigh@gmail.com>
2020-12-03 08:05:39 +1100

    changed * to / for home page route

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
 	app.get("/stats", (req, res) => {
 		res.sendFile(path.join(__dirname, "../public/stats.html"));
 	});
-	app.get("*", function (req, res) {
+	app.get("/", function (req, res) {
 		res.sendFile(path.join(__dirname, "../public/index.html"));
 	});
 };
