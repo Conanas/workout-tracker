89ef6d2494b713be1a59773702b375e2124665f5
Tom Haigh <tmhaigh@gmail.com>
2020-12-02 12:12:37 +1100

    set up exercise schema

--- models/exercise.js
+++ models/exercise.js
@@ -3,24 +3,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-    // name: {
-    //     type: String,
-    //     trim: true,
-    //     required: "Enter a name for transaction"
-    // },
-    // value: {
-    //     type: Number,
-    //     required: "Enter an amount"
-    // },
-    // date: {
-    //     type: Date,
-    //     default: Date.now
-    // }
-    // day: {
-    //     type: Date,
-    //     default: Date.now
-    // },
-    // exercises: 
+    type: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        require: true
+    },
+    weights: {
+        type: Number,
+        require: true
+    },
+    sets: {
+        type: Number,
+        require: true
+    },
+    reps: {
+        type: Number,
+        require: true
+    },
+    distance: {
+        type: Number,
+        require: true
+    }
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
