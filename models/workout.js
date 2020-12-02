e77d28375f33db1fc5baf44ef5dfd71768fcfedd
Tom Haigh <tmhaigh@gmail.com>
2020-12-02 15:05:02 +1100

    removed required

--- models/workout.js
+++ models/workout.js
@@ -3,17 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    exercises: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
-        }
-    ]
+	day: {
+		type: Date,
+		default: Date.now
+	},
+	exercises: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Exercise"
+		}
+	]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
