857ad3b764ae31616a185227198347d2ecd3a1cb
Tom Haigh <tmhaigh@gmail.com>
2020-12-03 08:05:58 +1100

    added get workouts, functional

--- controllers/workouts-controller.js
+++ controllers/workouts-controller.js
@@ -1,15 +1,34 @@
 let db = require("../models");
 
 module.exports = function (app) {
-    app.post("/api/workouts/", async function (req, res) {
-        try {
-            let workouts = await db.Workout.create(req.body);
-            res.json(workouts);
-        } catch (error) {
-            console.log(error);
-        }
-    });
-    app.put("/api/workouts/:id", function (req, res) {
-        // console.log(`put: ${req.body}`, req.params.id)
-    })
-}
+
+	app.get("/api/workouts", async function (req, res) {
+		try {
+			let workouts = await db.Workout.find().populate("exercises");
+			res.json(workouts);
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	// app.get("/api/workouts/range", function (req, res) {
+	// 	db.Workout.find().then(workouts => {
+	// 		res.json(workouts);
+	// 	}).catch(error => {
+	// 		res.json(error);
+	// 	});
+	// });
+
+	app.post("/api/workouts", async function (req, res) {
+		try {
+			let workouts = await db.Workout.create(req.body);
+			res.json(workouts);
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	// app.put("/api/workouts/:id", function (req, res) {
+
+	// });
+};
