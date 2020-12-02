5011b7c896839d874e6d81d44396aa349c0d5689
Tom Haigh <tmhaigh@gmail.com>
2020-12-02 13:43:44 +1100

    added route to stats.html

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,12 +1,15 @@
 let path = require("path");
 module.exports = function (app) {
-    // app.get("/exercise?", function (req, res) {
+	// app.get("/exercise?", function (req, res) {
 
-    // });
-    app.get("/exercise", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-}
+	// });
+	app.get("/exercise", function (req, res) {
+		res.sendFile(path.join(__dirname, "../public/exercise.html"));
+	});
+	app.get("/stats", (req, res) => {
+		res.sendFile(path.join(__dirname, "../public/stats.html"));
+	});
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "../public/index.html"));
+	});
+};
