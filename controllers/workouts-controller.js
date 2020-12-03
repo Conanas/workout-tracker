e30a8777162f324123b4a93b7185a31f0b60ba93
Tom Haigh <tmhaigh@gmail.com>
2020-12-03 14:30:51 +1100

    added get workouts range path

--- controllers/workouts-controller.js
+++ controllers/workouts-controller.js
@@ -11,13 +11,14 @@ module.exports = function (app) {
 		}
 	});
 
-	// app.get("/api/workouts/range", function (req, res) {
-	// 	db.Workout.find().then(workouts => {
-	// 		res.json(workouts);
-	// 	}).catch(error => {
-	// 		res.json(error);
-	// 	});
-	// });
+	app.get("/api/workouts/range", async (req, res) => {
+		try {
+			let workouts = await db.Workout.find().populate("exercises");
+			res.json(workouts);
+		} catch (error) {
+			console.log(error);
+		}
+	});
 
 	app.post("/api/workouts", async function (req, res) {
 		try {
