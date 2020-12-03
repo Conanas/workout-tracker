2ba4b5aa4808a68dc5f2a92d7944767fca9d2052
Tom Haigh <tmhaigh@gmail.com>
2020-12-03 11:57:08 +1100

    added totalDuration field and method to Schema

--- models/workout.js
+++ models/workout.js
@@ -12,8 +12,20 @@ const workoutSchema = new Schema({
 			type: Schema.Types.ObjectId,
 			ref: "Exercise"
 		}
-	]
+	],
+	totalDuration: {
+		type: Number
+	}
 });
+
+workoutSchema.methods.setTotalDuration = (exercises) => {
+	let acc = exercises.reduce((acc, curr) => {
+		acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
+		return acc;
+	}, {});
+	this.totalDuration = acc.totalDuration;
+	return this.totalDuration;
+};
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
